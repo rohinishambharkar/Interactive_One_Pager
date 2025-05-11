cce804d236da2715eb5d39eec66ec5cac3f1e87d
rohinishambharkar <rohinishambharkar2003@gmail.com>
2025-05-11 19:38:41 +0530

    generated draggable circle component

---
+++ src/app/draggable-circle/draggable-circle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draggable-circle',
+  imports: [],
+  templateUrl: './draggable-circle.component.html',
+  styleUrl: './draggable-circle.component.css'
+})
+export class DraggableCircleComponent {
+
+}
